1715f0836c55467d8615b29041c442933ca6d0e5
Xianweiwu <xianweics@163.com>
2020-01-12 18:49:31 +0800

    add publish.sh

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+npm config get registry
+npm config set registry=http://registry.npmjs.org
+
+echo 'Login http://registry.npmjs.org'
+npm login
+
+echo "Publish to http://registry.npmjs.org"
+npm publish
+
+npm config set registry=https://registry.npm.taobao.org
+echo -e "\nPublish successfully\n"
+exit
